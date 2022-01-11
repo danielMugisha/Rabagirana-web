cb48bdcafeeef4df3cc79d4fc766dfea36fd3359
Daniel MUGISHA <dmugisha189@gmail.com>
2022-01-12 00:03:21 +0200

    footer correction

--- src/components/Footers/Footer.js
+++ src/components/Footers/Footer.js
@@ -1,101 +1,101 @@
 import React from "react";
 
 export default function Footer() {
-  return (
-    <>
-      <footer className="relative bg-blueGray-200 pt-8 pb-6">
-        <div
-          className="bottom-auto top-0 left-0 right-0 w-full absolute pointer-events-none overflow-hidden -mt-20 h-20"
-          style={{ transform: "translateZ(0)" }}
-        >
-          <svg
-            className="absolute bottom-0 overflow-hidden"
-            xmlns="http://www.w3.org/2000/svg"
-            preserveAspectRatio="none"
-            version="1.1"
-            viewBox="0 0 2560 100"
-            x="0"
-            y="0"
-          >
-            <polygon
-              className="text-blueGray-200 fill-current"
-              points="2560 0 2560 100 0 100"
-            ></polygon>
-          </svg>
-        </div>
-        <div className="container mx-auto px-4">
-          <div className="flex flex-wrap text-center lg:text-left">
-            <div className="w-full lg:w-6/12 px-4">
-              <h4 className="text-3xl font-semibold">
-                Let's keep in touch with us!
-              </h4>
-              <h5 className="text-lg mt-0 mb-2 text-blueGray-600">
-                Find us on any of these platforms, we respond to you as soon as
-                possible.
-              </h5>
-              <div className="mt-6 lg:mb-0 mb-6">
-                <button
-                  className="bg-red-600 text-white w-8 h-8 rounded-full outline-none focus:outline-none mr-1 mb-1"
-                  type="button"
-                >
-                  <i className="fab fa-google"></i>
-                </button>
-                <button
-                  className="bg-lightBlue-400 text-white w-8 h-8 rounded-full outline-none focus:outline-none mr-1 mb-1"
-                  type="button"
-                >
-                  <i className="fab fa-twitter"></i>
-                </button>
-                <button
-                  className="bg-lightBlue-600 text-white w-8 h-8 rounded-full outline-none focus:outline-none mr-1 mb-1"
-                  type="button"
-                >
-                  <i className="fab fa-facebook-f"></i>
-                </button>
-              </div>
-            </div>
-            <div className="w-full lg:w-6/12 px-4">
-              <div className="flex flex-wrap items-top mb-6">
-                <div className="w-full lg:w-4/12 px-4 ml-auto">
-                  <span className="block uppercase text-blueGray-500 text-sm font-semibold mb-2">
-                    Links
-                  </span>
-                  <ul className="list-unstyled">
-                    <li>
-                      <a
-                        className="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm"
-                        href="/about"
-                      >
-                        About Rabagirana
-                      </a>
-                    </li>
-                    <li>
-                      <a
-                        className="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm"
-                        href="/stories"
-                      >
-                        Stories of impact
-                      </a>
-                    </li>
-                    <li>
-                      <a
-                        className="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm"
-                        href="/programs"
-                      >
-                        Programs
-                      </a>
-                    </li>
-                    <li>
-                      <a
-                        className="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm"
-                        href="/#"
-                      >
-                        COntact us
-                      </a>
-                    </li>
-                  </ul>
-                </div>
-                {/* <div className="w-full lg:w-4/12 px-4">
+	return (
+		<>
+			<footer className="relative bg-blueGray-200 pt-8 pb-6">
+				<div
+					className="bottom-auto top-0 left-0 right-0 w-full absolute pointer-events-none overflow-hidden -mt-20 h-20"
+					style={{ transform: "translateZ(0)" }}
+				>
+					<svg
+						className="absolute bottom-0 overflow-hidden"
+						xmlns="http://www.w3.org/2000/svg"
+						preserveAspectRatio="none"
+						version="1.1"
+						viewBox="0 0 2560 100"
+						x="0"
+						y="0"
+					>
+						<polygon
+							className="text-blueGray-200 fill-current"
+							points="2560 0 2560 100 0 100"
+						></polygon>
+					</svg>
+				</div>
+				<div className="container mx-auto px-4">
+					<div className="flex flex-wrap text-center lg:text-left">
+						<div className="w-full lg:w-6/12 px-4">
+							<h4 className="text-3xl font-semibold">
+								Let's keep in touch with us!
+							</h4>
+							<h5 className="text-lg mt-0 mb-2 text-blueGray-600">
+								Find us on any of these platforms, we respond to you as soon as
+								possible.
+							</h5>
+							<div className="mt-6 lg:mb-0 mb-6">
+								<button
+									className="bg-red-600 text-white w-8 h-8 rounded-full outline-none focus:outline-none mr-1 mb-1"
+									type="button"
+								>
+									<i className="fab fa-google"></i>
+								</button>
+								<button
+									className="bg-lightBlue-400 text-white w-8 h-8 rounded-full outline-none focus:outline-none mr-1 mb-1"
+									type="button"
+								>
+									<i className="fab fa-twitter"></i>
+								</button>
+								<button
+									className="bg-lightBlue-600 text-white w-8 h-8 rounded-full outline-none focus:outline-none mr-1 mb-1"
+									type="button"
+								>
+									<i className="fab fa-facebook-f"></i>
+								</button>
+							</div>
+						</div>
+						<div className="w-full lg:w-6/12 px-4">
+							<div className="flex flex-wrap items-top mb-6">
+								<div className="w-full lg:w-4/12 px-4 ml-auto">
+									<span className="block uppercase text-blueGray-500 text-sm font-semibold mb-2">
+										Links
+									</span>
+									<ul className="list-unstyled">
+										<li>
+											<a
+												className="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm"
+												href="/about"
+											>
+												About Rabagirana
+											</a>
+										</li>
+										<li>
+											<a
+												className="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm"
+												href="/stories"
+											>
+												Stories of impact
+											</a>
+										</li>
+										<li>
+											<a
+												className="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm"
+												href="/programs"
+											>
+												Programs
+											</a>
+										</li>
+										<li>
+											<a
+												className="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm"
+												href="/#"
+											>
+												Contact us
+											</a>
+										</li>
+									</ul>
+								</div>
+								{/* <div className="w-full lg:w-4/12 px-4">
                   <span className="block uppercase text-blueGray-500 text-sm font-semibold mb-2">
                     Other Resources
                   </span>
@@ -134,26 +134,26 @@ export default function Footer() {
                     </li>
                   </ul>
                 </div> */}
-              </div>
-            </div>
-          </div>
-          <hr className="my-6 border-blueGray-300" />
-          <div className="flex flex-wrap items-center md:justify-between justify-center">
-            <div className="w-full md:w-4/12 px-4 mx-auto text-center">
-              <div className="text-sm text-blueGray-500 font-semibold py-1">
-                Copyright © {new Date().getFullYear()} Rabagirana by{" "}
-                <a
-                  href="/#"
-                  className="text-blueGray-500 hover:text-blueGray-800"
-                >
-                  PickLabs
-                </a>
-                .
-              </div>
-            </div>
-          </div>
-        </div>
-      </footer>
-    </>
-  );
+							</div>
+						</div>
+					</div>
+					<hr className="my-6 border-blueGray-300" />
+					<div className="flex flex-wrap items-center md:justify-between justify-center">
+						<div className="w-full md:w-4/12 px-4 mx-auto text-center">
+							<div className="text-sm text-blueGray-500 font-semibold py-1">
+								Copyright © {new Date().getFullYear()} Rabagirana by{" "}
+								<a
+									href="/#"
+									className="text-blueGray-500 hover:text-blueGray-800"
+								>
+									Peak-Labs
+								</a>
+								.
+							</div>
+						</div>
+					</div>
+				</div>
+			</footer>
+		</>
+	);
 }
