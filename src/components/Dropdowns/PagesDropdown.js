9b2ece088096c63120e1e074bc315f460e992965
Daniel MUGISHA <dmugisha189@gmail.com>
2022-01-12 00:24:31 +0200

    netlify errors

--- src/components/Dropdowns/PagesDropdown.js
+++ src/components/Dropdowns/PagesDropdown.js
@@ -1,72 +1,71 @@
 import React from "react";
-import classNames from "classnames";
 import { Link } from "react-router-dom";
 import { createPopper } from "@popperjs/core";
 
 const PagesDropdown = () => {
-  // dropdown props
-  const [dropdownPopoverShow, setDropdownPopoverShow] = React.useState(false);
-  const btnDropdownRef = React.createRef();
-  const popoverDropdownRef = React.createRef();
-  const openDropdownPopover = () => {
-    createPopper(btnDropdownRef.current, popoverDropdownRef.current, {
-      placement: "bottom-start",
-    });
-    setDropdownPopoverShow(true);
-  };
-  const closeDropdownPopover = () => {
-    setDropdownPopoverShow(false);
-  };
-  return (
-    <>
-      <a
-        className="lg:text-white lg:hover:text-yellow-400 text-blueGray-700 px-3 py-4 lg:py-2 flex items-center text-xs uppercase font-bold"
-        href="#pablo"
-        ref={btnDropdownRef}
-        onClick={(e) => {
-          e.preventDefault();
-          dropdownPopoverShow ? closeDropdownPopover() : openDropdownPopover();
-        }}
-      >
-        Programs
-      </a>
-      <div
-        ref={popoverDropdownRef}
-        className={
-          (dropdownPopoverShow ? "block " : "hidden ") +
-          "bg-white text-base z-50 float-left py-2 list-none text-left rounded shadow-lg min-w-48"
-        }
-      >
-        <Link
-          to="/programs/peacebuilding"
-          className={
-            "text-sm py-2 px-4 font-normal block w-full whitespace-nowrap bg-transparent text-blueGray-700"
-          }
-        >
-          <i className="fas fa-suitcase mr-2 text-lg text-blueGray-400"></i>{" "}
-          PEACE-BUILIDING, HEALING AND RECONCILIATION
-        </Link>
-        <Link
-          to="/programs/community-development"
-          className={
-            "text-sm py-2 px-4 font-normal block w-full whitespace-nowrap bg-transparent text-blueGray-700"
-          }
-        >
-          <i className="fas fa-users mr-2 text-lg text-blueGray-400"></i>{" "}
-          COMMUNITY DEVELOPMENT
-        </Link>
-        <Link
-          to="/programs/leadership-development"
-          className={
-            "text-sm py-2 px-4 font-normal block w-full whitespace-nowrap bg-transparent text-blueGray-700"
-          }
-        >
-          <i className="fas fa-user mr-2 text-lg text-blueGray-400"></i>{" "}
-          LEADERSHIP DEVELOPMENT
-        </Link>
-      </div>
-    </>
-  );
+	// dropdown props
+	const [dropdownPopoverShow, setDropdownPopoverShow] = React.useState(false);
+	const btnDropdownRef = React.createRef();
+	const popoverDropdownRef = React.createRef();
+	const openDropdownPopover = () => {
+		createPopper(btnDropdownRef.current, popoverDropdownRef.current, {
+			placement: "bottom-start",
+		});
+		setDropdownPopoverShow(true);
+	};
+	const closeDropdownPopover = () => {
+		setDropdownPopoverShow(false);
+	};
+	return (
+		<>
+			<a
+				className="lg:text-white lg:hover:text-yellow-400 text-blueGray-700 px-3 py-4 lg:py-2 flex items-center text-xs uppercase font-bold"
+				href="#pablo"
+				ref={btnDropdownRef}
+				onClick={(e) => {
+					e.preventDefault();
+					dropdownPopoverShow ? closeDropdownPopover() : openDropdownPopover();
+				}}
+			>
+				Programs
+			</a>
+			<div
+				ref={popoverDropdownRef}
+				className={
+					(dropdownPopoverShow ? "block " : "hidden ") +
+					"bg-white text-base z-50 float-left py-2 list-none text-left rounded shadow-lg min-w-48"
+				}
+			>
+				<Link
+					to="/programs/peacebuilding"
+					className={
+						"text-sm py-2 px-4 font-normal block w-full whitespace-nowrap bg-transparent text-blueGray-700"
+					}
+				>
+					<i className="fas fa-suitcase mr-2 text-lg text-blueGray-400"></i>{" "}
+					PEACE-BUILIDING, HEALING AND RECONCILIATION
+				</Link>
+				<Link
+					to="/programs/community-development"
+					className={
+						"text-sm py-2 px-4 font-normal block w-full whitespace-nowrap bg-transparent text-blueGray-700"
+					}
+				>
+					<i className="fas fa-users mr-2 text-lg text-blueGray-400"></i>{" "}
+					COMMUNITY DEVELOPMENT
+				</Link>
+				<Link
+					to="/programs/leadership-development"
+					className={
+						"text-sm py-2 px-4 font-normal block w-full whitespace-nowrap bg-transparent text-blueGray-700"
+					}
+				>
+					<i className="fas fa-user mr-2 text-lg text-blueGray-400"></i>{" "}
+					LEADERSHIP DEVELOPMENT
+				</Link>
+			</div>
+		</>
+	);
 };
 
 export default PagesDropdown;
